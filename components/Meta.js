b7e38c209b151c07b2ac4600dbe673e87c53b6e9
Yuri Brunetto <brunettoyuri@gmail.com>
2018-03-31 13:26:50 -0300

    initial commit and configuration

---
+++ components/Meta.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import '../styles/main.scss'
+
+export default () => (
+  <div>
+    <Head>
+      <title>moonlight 🌚</title>
+    </Head>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Meta from '../components/Meta'
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+
+        <main>
+          <h1>moonlight 🌚</h1>
+        </main>
+      </div>
+    )
+  }
+}
